ca39da9b0d2d8cc297fa8dfe7b80dc8333f47702
Van0SS <Van0SS@users.noreply.github.com>
2018-05-05 09:51:59 -0400

    Update init.sh
    Clear the context

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@
 ( sleep 30 ; \
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
 rabbitmqctl set_user_tags $RABBITMQ_USER user ; \
-# Only write and read for am.plugin exchanges
-rabbitmqctl set_permissions -p / $RABBITMQ_USER  "^am.plugin.*" "^am.plugin.*" "^am.plugin.*" ; \
+# Only write and read for needed.permissions exchanges
+rabbitmqctl set_permissions -p / $RABBITMQ_USER  "^needed.permissions*" "^needed.permissions.*" "^needed.permissions.*" ; \
 echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
 echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***";) &
 
